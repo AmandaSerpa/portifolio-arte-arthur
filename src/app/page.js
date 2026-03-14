c23be0ce0716c30c8ec4740acc6ec492f1d05223
Amanda <amanda.paula1999@hotmail.com>
2026-03-13 21:48:08 -0300

    Supabase
    Criação do storage e do bd pela supabase.
    Configuração pra conexão do next com supabase

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,9 @@
+import {supabase} from "@/lib/supabase";
 import Image from "next/image";
 import Link from "next/link";
 
-export default function Home() {
+
+export default async function Home() {
   return (
     <main className="bg-[#243415]">
       
